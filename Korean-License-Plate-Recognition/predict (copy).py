7d727c506fbfb372be3391432a2630cbc9193d70
윤창섭 <ckdtjq202001@gmail.com>
2020-08-22 15:31:55 +0900

    Update predict (copy).py

--- Korean-License-Plate-Recognition/predict (copy).py
+++ Korean-License-Plate-Recognition/predict (copy).py
@@ -21,90 +21,153 @@ classnames = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 
 
 def detection() :
-    img_color = cv2.imread('1234.jpg', cv2.IMREAD_COLOR)
-    img_color = cv2.resize(img_color, dsize=(2688,1520), interpolation=cv2.INTER_AREA)
-
+    now=datetime.datetime.now()
+    nowDatetime = now.strftime('%Y-%m-%d %H- %M-%S ')
+    
+    img_color = cv2.imread("1234.jpg", cv2.IMREAD_COLOR)
+    img_color = cv2.resize(img_color,dsize=(2688,1520),interpolation=cv2. INTER_AREA)
+    
+    height,width,channel = img_color.shape
+    
     copy_img=img_color.copy()
-    img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(img_gray, (3,3), 2)
+    
+    gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3,3), 3)
     canny = cv2.Canny(blur, 100, 100)
-
-    # contours로 같은에너지 네모박스
-    #contours,hierarchy = cv2.findContours(canny, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
-
-    _, contours,_=cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
+    contours,hierarchy = cv2.findContours(canny, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+    
+    
+    #box1은 번호판 크기 ,box2 는 번호판안의 숫자 크기
     box1=[]
-
-    f_count=0
-    select=0
-    plate_width=0
-
+    box2=[]
+    
+    #신형 및 구형 번호판 비율을 고려하여 설정하였다. 
+    # 번호판 전체 크기를 고려하여 설정하였다. 거리에 맞추어 설정해야함 옛날 번호판은 인식 못하는걸 생각
+    NEW_CAR_ratio=9
+    OLD_CAR_ratio=2.5
+    MAX_plate_area=height*width*0.002
+    MIN_plate_area=1000
+    
+    #번호판 안의 숫자의 최소크기, 너비 ,길이 ,비율 을 설정하였다.
+    NUM_MIN_AREA = 100
+    NUM_MIN_WIDTH, NUM_MIN_HEIGHT =2 , 8
+    NUM_MIN_RATIO , NUM_MAX_RATIO = 0.2 ,1 
+    
+    # box1 box2 제조
     for i in range(len(contours)):
         cnt=contours[i]
         area = cv2.contourArea(cnt)
         x,y,w,h = cv2.boundingRect(cnt)
         rect_area=w*h
-        aspect_ratio = float(w)/h
-
-        if (aspect_ratio >= 0.2) and (aspect_ratio<=1.0) and (rect_area>=100) and (rect_area<=2400):
+        aspect_ratio1 = float(w)/h
+    
+    
+        if (aspect_ratio1 <= NEW_CAR_ratio) and (aspect_ratio1>=OLD_CAR_ratio) and (rect_area>MIN_plate_area) and (rect_area<MAX_plate_area):
+    
             cv2.rectangle(img_color,(x,y),(x+w, y+h), (0, 255,0), 1)
             box1.append(cv2.boundingRect(cnt))
-
-    # 버블정렬
+    
+        if (aspect_ratio1 >= NUM_MIN_RATIO) and (aspect_ratio1<=NUM_MAX_RATIO) and (rect_area >=NUM_MIN_AREA) :
+            cv2.rectangle(img_color,(x,y),(x+w, y+h), (255, 0,0), 1)
+            box2.append(cv2.boundingRect(cnt))
+    
+    
+    cv2.imwrite("boxing.jpg",img_color)
+    
+    # box1 버블정렬
     for i in range(len(box1)): 
-        for j in range(len(box1)-(i+1)):
-            if box1[j][0]>box1[j+1][0]:
-                temp=box1[j]
-                box1[j]=box1[j+1]
-                box1[j+1]=temp
+         for j in range(len(box1)-(i+1)):
+              if box1[j][0]>box1[j+1][0]:
+                   temp=box1[j]
+                   box1[j]=box1[j+1]
+                   box1[j+1]=temp
+    
+    
+    # box2 버블정렬
+    for i in range(len(box2)): 
+         for j in range(len(box2)-(i+1)):
+              if box2[j][0]>box2[j+1][0]:
+                   temp=box2[j]
+                   box2[j]=box2[j+1]
+                   box2[j+1]=temp
+    
+    
+    #box1 에서 겹치는 부분 제거  미완성 부분 추가적인 수정 필요
+    box3=[]
+    for i in range(len(box1)-1):
+                
+        if (box1[i][0]+box1[i][2]) <= (box1[i+1][0]+box1[i+1][2]) and (box1[i][0]+box1[i][2])>=(box1[i+1][0]) and (box1[i][1]+box1[i][3])<=(box1[i+1][1]+box1[i+1][3]) and (box1[i][1]+box1[i][3])>=(box1[i+1][1]) : 
+            box1[i]=0
+        else:        
+            box3.append(box1[i])
+    box1=box3
+    
+    
+    # #번호판 인식과정 box1에서 box2에 있는게 5개 정도 포함이 되는 경우 번호판이라 칭함 # 중심으로 생각새헛 ㅗㅇㄹ
+    # select=[]
+    # for i in range(len(box1)):
+    #     count=0
+    #     for j in range(len(box2)):
+    #         if box1[i][0]<box2[j][0] and (box1[i][0]+box1[i][2])>box2[j][0] and box1[i][1]<box2[j][1]  and (box1[i][1]+box1[i][3])>box2[j][1] :
+    #             count=count+1
+    #     if count >=5:
+    #         select.append(i)
+    
+    
+    select=[]
+    for i in range(len(box1)):
+        count=0
+        for j in range(len(box2)):
+            if box1[i][0]<box2[j][0]+box2[j][2]/2 and (box1[i][0]+box1[i][2])>box2[j][0]+box2[j][2]/2 and box1[i][1]<box2[j][1]+box2[j][3]/2  and (box1[i][1]+box1[i][3])>box2[j][1]+box2[j][3]/2 :
+                count=count+1
+        if count >=5:
+            select.append(i)
+    
+    # #번호판 겹칠때 설정 나중에 따로 수정 
+    
+    # for i in range(len(select)-1):
+    #     if box1[select[i]][0] == box1[select[i+1]][0] or box1[select[i]][1] == box1[select[i+1]][1] or box1[select[i]][2] == box1[select[i+1]][2] or box1[select[i]][3] == box1[select[i+1]][3]:
+    #         select_temp.append(select[i])
+    
+    select_temp=[]
+    cnt=[0,0,0]
+    #차량 인식 과정
+    for i in range(len(select)):
+        if  box1[select[i]][0]<width*0.33:
+    
+            if cnt[0]==0:
+            
+                select_temp.append(select[i])
+                cnt.append(1)
+     
+        elif box1[select[i]][0]<width*0.66:        
+            if cnt[1]==0:
+              
+                select_temp.append(select[i])
+                cnt.append(2)
+     
+        elif box1[select[i]][0]<width:
+            if cnt[2]==0:
+         
+                select_temp.append(select[i])
+                cnt.append(3)
+    
+    select=select_temp
+    
+    #번호판 출력과정 
+    number_plate=[]
+    for i in range(len(select)):
+        number_plate.append(0)
+    
+    for i in range(len(select)):
+        number_plate[i]=(copy_img[box1[select[i]][1]        :     box1[select[i]][3]+      box1[select[i]][1]  ,        box1[select[i]][0]   :  box1[select[i]][0]  + box1[select[i]][2]                    ])
+        cv2.imshow("show",number_plate[i])
+        cv2.waitKey(0)
+    
+    for i in range(len(select)):
+        cv2.imwrite(str(nowDatetime)+"plate_"+str(cnt[i+3])+".jpg",number_plate[i])
 
-    box1_1 = [[0 for j in range(4)] for i in range(len(box1))]
-    box1_2 = [[0 for j in range(4)] for i in range(len(box1))]
-    box1_3 = [[0 for j in range(4)] for i in range(len(box1))]
-
-    for k in range(len(box1)):     
-        if box1[k][0]<800:
-            box1_1[k]=box1[k]
-        elif box1[k][0]<1600:
-            box1_2[k]=box1[k]
-        elif box1[k][0]<2600:
-            box1_3[k]=box1[k]
-
-    box1=[box1_1,box1_2,box1_3]
-    select=[0,0,0]
-
-    for i in range(0,3):
-        f_count=0
-        for m in range(len(box1[i])):
-            count=0
-            for n in range(m+1,(len(box1[i])-1)):
-                delta_x=abs(box1[i][n+1][0]-box1[i][m][0])
-                if delta_x > 140:
-                        break
-                delta_y =abs(box1[i][n+1][1]-box1[i][m][1])
-                if delta_x ==0:
-                        delta_x=1
-                if delta_y ==0:
-                        delta_y=1           
-                gradient =float(delta_y) /float(delta_x)
-                        
-                if gradient<0.25:
-                        count=count+1
-                    
-            #measure number plate size         
-            if count > f_count :
-                f_count=count 
-                select[i] = m
-                plate_width=delta_x
-
-
-
-    number_plate_1=copy_img[box1[0][select[0]][1]-10:box1[0][select[0]][3]+box1[0][select[0]][1]+5,box1[0][select[0]][0]-5:180+box1[0][select[0]][0]]
-    number_plate_2=copy_img[box1[1][select[1]][1]-10:box1[1][select[1]][3]+box1[1][select[1]][1]+5,box1[1][select[1]][0]-5:180+box1[1][select[1]][0]]
-    number_plate_3=copy_img[box1[2][select[2]][1]-10:box1[2][select[2]][3]+box1[2][select[2]][1]+5,box1[2][select[2]][0]-5:180+box1[2][select[2]][0]]
-
-    return (number_plate_1, number_plate_2, number_plate_3)
+    return (number_plate[0], number_plate[1], number_plate[2])
 
     # cv2.imshow('1', number_plate_1)
     # cv2.waitKey(0)
